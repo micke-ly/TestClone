645c1f53a9ea22e7aae23dd3fbc729d63d4343e7
mickelyweb <mickelyweb@gmail.com>
2023-11-13 10:52:52 +0100

    Change image based on player choice

--- js/normal-rps.js
+++ js/normal-rps.js
@@ -1,6 +1,20 @@
+// SELECTORS
 const modalToggleBtns = document.querySelectorAll('.modal-toggle');
 const modal = document.getElementById('modal');
+const playerChoices = document.querySelector('.rps-btn-container');
+const showPlayerMove = document.getElementById('show-player-move');
 
+// VARIABLES
+let playerMove;
+let computerMove;
+
+playerChoices.addEventListener('click', (event) => {
+  playerMove = event.target.dataset.id;
+  showPlayerMove.src = `images/${playerMove}.png`;
+  console.log(playerMove);
+});
+
+// MODAL
 modalToggleBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
     modal.classList.toggle('modal-inactive');
